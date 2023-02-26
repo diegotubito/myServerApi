671a431c21c113f840902e0c960ab5ad5ec70b47
David Diego Gomez <diegodavid@icloud.com>
2023-02-26 10:37:18 -0300

    Validation Errors

--- Router/user_router.js
+++ Router/user_router.js
@@ -1,9 +1,16 @@
 const { Router } = require('express')
 const router = Router()
 const {userGet, userPost, userUpdate, userDelete} = require('../Controller/user_controller')
+const {check} = require('express-validator')
+const checkValidationResult = require('../Middleware/Error/validation_error')
 
 router.get('/user', userGet)
-router.post('/user', userPost)
+router.post('/user', [
+    check('displayName', 'Display name is too short or too long').isLength({min: 2, max: 30}),
+    check('email', 'Email format is invalid').isEmail(),
+    check('password', 'Password format is wrong').isLength({min:4}),
+    checkValidationResult
+], userPost)
 router.put('/user', userUpdate)
 router.delete('/user', userDelete)
 
