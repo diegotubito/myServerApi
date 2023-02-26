10b3a35344f41f78fb549e87d88fa63163d7555e
David Gomez <davidgomez@Davids-Air.fibertel.com.ar>
2023-02-25 21:15:29 -0300

    Connected to database

--- Model/server.js
+++ Model/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 class Server {
     constructor() {
@@ -21,7 +22,19 @@ class Server {
     connect() {
         this.app.listen(3000, () => {
             console.log('Server running at 3000')
+            this.connectDB()
         })
+    }
+
+    connectDB() {
+        mongoose.set('strictQuery', true)
+        mongoose.connect('mongodb+srv://cafe:Xx4109SOReN1DLR8@cluster0.jfb6n.mongodb.net/temperature', (error, response) => {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log('database connected')
+            }
+       })
     }
 }
 
